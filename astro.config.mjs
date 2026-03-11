06afa603869b823f53f7d671f8b8b904bfb52565
Inyoung <inyoungcheong@gmail.com>
2026-03-11 16:18:30 -0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
+export default defineConfig({
+  site: 'https://inyoungcheong.github.io',
+  base: '/research-statemnt',
+  integrations: [
+    tailwind(),
+    mdx({
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+      ],
+    }),
+  ],
+});
